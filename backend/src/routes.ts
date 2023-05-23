5a752e4d5cfec282edbe58d09dcfa3ef64dc8462
Henrique Costa <henrysilva14@gmail.com>
2023-05-23 14:52:10 -0300

    create auth and user routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,11 @@
 import { Router, Request, Response } from "express";
+import { CreateUserController } from './controllers/user/CreateUserController';
+import { AuthUserController } from './controllers/user/AuthUserController';
 
 const router = Router();
 
-router.get('/teste', (req: Request, res: Response) => {
-    return res.json({nome: 'Chico Bento'})
-});
+//Rotas user
+router.post('/users', new CreateUserController().handle)
+router.post('/session', new AuthUserController().handle)
 
 export { router }
-
---
+++ backend/src/services/user/AuthUserService.ts
@@ -0,0 +1,31 @@
+import prismaClient from "../../prisma";
+import { compare } from 'bcryptjs';
+
+interface AuthRequest{
+    email: string;
+    password: string;
+}
+
+class AuthUserService{
+    async execute({email, password}: AuthRequest) {
+        //Verificar se o email existe
+        const user = await prismaClient.user.findFirst({
+            where: {email: email}
+        })
+
+        if (!user) {
+            throw new Error("User/password incorrect")
+        }
+
+        // Preciso verificar se a senha que ele mandou está correta
+        const passwordMatch = await compare(password, user.password)
+        if (!passwordMatch) {
+            throw new Error("User/password incorrect")
+        }
+
+        // Gerar um token JWT e devolver os dados do usuário como id, name e email
+        return {ok: true}
+    }
+}
+
+export { AuthUserService };
